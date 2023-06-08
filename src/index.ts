426c6a55b70a85af3435fdbaead4b658c3e5aca7
Raquel Adrados <radrados00@gmail.com>
2023-06-08 19:31:55 +0200

    Change index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
-// http://localhost:8000/calculator?a=6&b=3
 import http from 'http';
+import url from 'url';
 import { program } from 'commander';
 import * as dotenv from 'dotenv';
 
+dotenv.config();
+
 const PORT = process.env.PORT || 2000;
-const server = http.createServer((_request, response) => {
-  response.write('hola');
+const version = '1.0.0';
+
+program.option('-v, --version');
+program.parse();
+const options = program.opts();
+
+if (options.version) {
+  console.log('Version ' + version);
+  process.exit(0);
+}
+
+const server = http.createServer((req, res) => {
+  if (!req.url) {
+    server.emit('error', new Error('No url in the request'));
+    return;
+  }
+
+  const { pathname } = url.parse(req.url);
+
+  if (req.method !== 'GET') {
+    server.emit('error', new Error('Invalid method'));
+    return;
+  }
+
+  res.write(`<h1>Hola ${pathname!.toUpperCase()}</h1>`);
+  res.write(req.method);
+  res.write(req.url);
+  res.end();
 });
+
 server.listen(PORT);
+
+server.on('listening', () => {
+  console.log('Listening on port ' + PORT);
+});
+
+server.on('error', (error) => {
+  console.log(error.message);
+});
